d89c03a8d2ee0b0d946821be4f5c20ae5a0ec7d9
Crystal Broach <crysland@gmail.com>
2020-05-12 17:59:31 -0500

    setup completed

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-danger">Some Cool Button</button>
+    </div>
+  );
+}
+
+export default App;
