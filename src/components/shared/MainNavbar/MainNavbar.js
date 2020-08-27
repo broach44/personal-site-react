9ad02f1cdb87f46854f56f2a7e584dcb62e492be
Crystal Broach <crysland@gmail.com>
2020-08-26 22:35:25 -0500

    resume added to open in new tab

--- src/components/shared/MainNavbar/MainNavbar.js
+++ src/components/shared/MainNavbar/MainNavbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import resume from '../../../helpers/assets/CrystalBroach_Resume.pdf';
+
 import './MainNavbar.scss';
 
 class MainNavbar extends React.Component {
@@ -8,7 +10,7 @@ class MainNavbar extends React.Component {
       <div className="MainNavbar">
         <div className="navLink">Projects</div>
         <div className="navLink">Technologies</div>
-        <div className="navLink">Resume</div>
+        <div className="navLink"><a href={resume} target="_blank">Resume</a></div>
       </div>
     );
   }
