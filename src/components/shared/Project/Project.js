427e1066dbb2906d71936c1e725414505d461533
Crystal Broach <crysland@gmail.com>
2020-08-30 12:31:02 -0500

    projects on page

--- src/components/shared/Project/Project.js
+++ src/components/shared/Project/Project.js
@@ -1,19 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './Project.scss';
 
 class Project extends React.Component {
+  static props = {
+    project: PropTypes.obj,
+  }
+
+  renderLiveLink = () => {
+    const { project } = this.props;
+    if (project.url !== '') {
+      return (<a href={project.url}>Click Here to View Demo</a>);
+    }
+    return null;
+  }
+
   render() {
+    const { project } = this.props;
     return (
       <div className="Project">
         <div>
-          <img src="https://image.freepik.com/free-photo/computer_1205-717.jpg?1" alt="" />
+          <img className="projectImage" src={project.screenshot} alt="" />
         </div>
         <div className="ProjectInfo">
-          <h4>Title of Project</h4>
-          <p>Tech Used</p>
-          <p>Include a brief description of the project</p>
-          <div>More Info Button</div>
+          <h4>{project.title}</h4>
+          <p>Tech Stack: {project.technologiesUsed}</p>
+          <p>Description: {project.description}</p>
+          <p>
+          <a href={project.githubUrl}>Click to View Code</a>
+          </p>
+          { this.renderLiveLink() }
+          {/* <div>More Info Button</div> */}
         </div>
       </div>
     );
