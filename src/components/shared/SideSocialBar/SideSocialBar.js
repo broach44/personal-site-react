df0dc53f025e4ae5676592445ab19f6564acd1f4
Crystal Broach <crysland@gmail.com>
2020-08-30 13:18:04 -0500

    affixed the social bar

--- src/components/shared/SideSocialBar/SideSocialBar.js
+++ src/components/shared/SideSocialBar/SideSocialBar.js
@@ -1,6 +1,4 @@
 import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { facebook } from '@fortawesome/free-solid-svg-icons';
 
 import './SideSocialBar.scss';
 
@@ -8,19 +6,18 @@ class SideSocialBar extends React.Component {
   render() {
     return (
       <div className="SideSocialBar">
-        <h4>Contact Info:</h4>
         <ul>
           <li>
-            <a href="mailto: crysland@gmail.com" className="email socialLink">Email</a>
+            <a href="mailto: crysland@gmail.com" className="email socialLink"><i class="fas fa-paper-plane"></i>  Email</a>
           </li>
           <li>
-            <a href="https://www.linkedin.com/in/crystal-broach" className="linkedin socialLink">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/crystal-broach" className="linkedin socialLink"><i class="fab fa-linkedin"></i>  LinkedIn</a>
           </li>
           <li>
-            <a href="https://github.com/broach44" className="gitHub socialLink">Github</a>
+            <a href="https://github.com/broach44" className="gitHub socialLink"><i class="fab fa-github-square"></i>  Github</a>
           </li>
           <li>
-            <a href="https://twitter.com/crystal_1344?lang=en" className="twitter socialLink">Twitter</a>
+            <a href="https://twitter.com/crystal_1344?lang=en" className="twitter socialLink"><i class="fab fa-twitter-square"></i>  Twitter</a>
           </li>
         </ul>
       </div>
